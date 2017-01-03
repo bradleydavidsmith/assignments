9efe50fd058c103ad25c9554103d314114808876
bsmith <mytoureasy@gmail.com>
2017-01-02 16:55:51 -0800

    Trying to track down problem

---
+++ C#/23/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill2
+{
+    class ModA
+    {
+        private static void A()
+        {
+            Console.WriteLine("ModA A");
+        }
+
+        public static void B()
+        {
+            Console.WriteLine("ModA B");
+            A();
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                // B is public, so this works
+                ModA.B();
+                // A is marked private, so this should result in an error:
+                Console.WriteLine("This should be an error:");
+                ModA.A();
+            }
+        }
+    }
+}

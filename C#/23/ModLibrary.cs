7da78dd43f6c716d11422ab9d6c5b84deac6ce54
bsmith <mytoureasy@gmail.com>
2017-01-02 18:58:06 -0800

    Interna keyword problem

---
+++ C#/23/ModLibrary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// Internal Class with public method
+namespace ModLibrary
+{
+    internal class ModLibA
+    {
+        public void MethodA() { }
+    }
+}

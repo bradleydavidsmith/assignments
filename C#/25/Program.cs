caf7380dfbf58ea957adf36e3d50d522639be85f
bsmith <mytoureasy@gmail.com>
2017-01-07 09:31:19 -0800

    Step 25 Drill

---
+++ C#/25/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Difference between string and StringBuilder:
+            // strings are immutable, StringBuilder is mutable
+            // Immutable objects have some good properties,
+            // But can cause performance and memory problems:
+
+            // This example creates 1002 strings, of which 1000 are
+            // thrown away
+
+            string s = String.Empty;
+            for (var i = 0; i < 1000; i++)
+            {
+                s += i.ToString() + " ";
+            }
+
+            // This is the same example, using StringBuilder. 
+            // Note that only 1 StringBuilder is created, along
+            // with 1 string.
+
+            StringBuilder sb = new StringBuilder();
+            for (var i = 0; i < 1000; i++)
+            {
+                sb.Append(i);
+                sb.Append(" ");
+            }
+
+            Console.WriteLine("s = " + s);
+            Console.WriteLine("sb = " + sb);
+        }
+    }
+}
